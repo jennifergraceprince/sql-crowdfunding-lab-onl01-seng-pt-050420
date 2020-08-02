7f008f9d805cf737a606b7a6b8e6505402be13ee
Jennifer Prince <jennifergraceprince@gmail.com>
2020-08-02 21:52:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -61,3 +61,12 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   GROUP BY projects.id
   ORDER BY projects.title ASC;"
 end
+
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
+  "SELECT users.name, users.age, SUM(pledges.amount)
+  FROM users
+  INNER JOIN pledges
+  ON users.id = pledges.user_id
+  GROUP BY users.name
+  ORDER BY users.name ASC;"
+end
